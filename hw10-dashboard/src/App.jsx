d6f8098116967e66510a5eb557f84cb4e03b9894
LelandJones <lelandj7@outlook.com>
2026-04-22 14:30:33 -0400

    commit

--- hw10-dashboard/src/App.jsx
+++ hw10-dashboard/src/App.jsx
@@ -64,8 +64,10 @@ export default function App() {
 
       <p><strong>Username:</strong> {data.login}</p>
       <p><strong>Name:</strong> {data.name}</p>
+      <p><strong>Company:</strong> {data.company}</p>
       <p><strong>Public Repos:</strong> {data.public_repos}</p>
       <p><strong>Followers:</strong> {data.followers}</p>
+
 
       <a href={data.html_url} target="_blank">
         View Profile
